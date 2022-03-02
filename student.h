e32a2719664edab20f3e4e33014c19719c05418b
fahimmahmud618 <75488772+fahimmahmud618@users.noreply.github.com>
2022-03-02 20:49:01 +0600

    Create student.h

---
+++ student.h
@@ -0,0 +1,13 @@
+#ifndef STUDENT_H_INCLUDED
+#define STUDENT_H_INCLUDED
+#include "header.h"
+using namespace std;
+
+class student:public person
+{
+    int current_semister;
+
+};
+
+
+#endif // STUDENT_H_INCLUDED
