00bae6dcbe2a894e7e7ff6970576fb0964b38b63
fahimmahmud618 <75488772+fahimmahmud618@users.noreply.github.com>
2022-03-03 20:16:59 +0600

    Update header.h

--- header.h
+++ header.h
@@ -5,12 +5,21 @@
 #include <fstream>
 #include <windows.h>
 #include <conio.h>
+#include <string>
+#include <cstring>
+#include <direct.h>
 #include "person.h"
 #include "student.h"
+#include "semister_course.h"
 
 using namespace std;
 
 void login();
 void gotoxy(int x, int y);
+void color(int color);
+
+void home_admin();
+    void add_student();
+
 
 #endif // HEADER_H_INCLUDED
