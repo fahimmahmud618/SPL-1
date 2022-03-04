aa881bb469f0b1012eaae56f7403e810b75e3803
fahimmahmud618 <75488772+fahimmahmud618@users.noreply.github.com>
2022-03-04 12:21:54 +0600

    Update header.h

--- header.h
+++ header.h
@@ -1,4 +1,3 @@
-#ifndef HEADER_H_INCLUDED
 #define HEADER_H_INCLUDED
 
 #include <iostream>
@@ -22,11 +21,10 @@ void home_admin();
     void home_admin_control_teacher();
     void home_admin_control_student();
         void add_student();
-        void modify_student();
+        void modify_student(int batch_serial, int student_id);
         void delete_student();
         void view_student_info();
     void home_admin_control_reports();
-
 
 
 #endif // HEADER_H_INCLUDED
