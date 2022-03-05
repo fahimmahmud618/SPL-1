b0cfb31da6c159032a2510c00aae1418f1c797c6
fahimmahmud618 <75488772+fahimmahmud618@users.noreply.github.com>
2022-03-05 17:10:04 +0600

    Update home_admin.cpp

--- home_admin.cpp
+++ home_admin.cpp
@@ -115,6 +115,47 @@ void home_admin_control_student()
 
 void home_admin_control_reports()
 {
+    int choice;
+    system("cls");
 
+    gotoxy(5,3);
+    cout<<"Admin Home :: Ve";
+    gotoxy(10,5);
+    cout<<"1.Reports from teacher";
+    gotoxy(10,6);
+    cout<<"2.Reports from students";
+
+    gotoxy(10,12);
+    cout<<"Enter your choice: ";
+    gotoxy(40,12);
+    cin>>choice;
+
+    if(choice==1)
+    {
+        ifstream file(".//reports//teacher_to_admin.txt");
+        if(file.is_open())
+        {
+            system("cls");
+            string line_in_file;
+            while(getline(file,line_in_file))
+            {
+                cout<<line_in_file<<endl;
+            }
+        }
+    }
+
+    if(choice==2)
+    {
+        ifstream file(".//reports//student_to_admin.txt");
+        if(file.is_open())
+        {
+            system("cls");
+            string line_in_file;
+            while(getline(file,line_in_file))
+            {
+                cout<<line_in_file<<endl;
+            }
+        }
+    }
 }
 
