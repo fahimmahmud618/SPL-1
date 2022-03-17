5f0e6efa032b90342fae070d34f5d085481d44ee
fahimmahmud618 <75488772+fahimmahmud618@users.noreply.github.com>
2022-03-17 23:47:14 +0600

    Update control_teacher.cpp

--- control_teacher.cpp
+++ control_teacher.cpp
@@ -7,26 +7,18 @@ void home_admin_control_teacher()
     string makefilename,teacher_id;
     char* filename;
 
-    system("cls");
-    gotoxy(5,3);
-    cout<<"Admin :: Teacher Control";
+    string window_option[10];
+    basic_window();
+    window_option[0]="Add Teacher";
+    window_option[1]="Modify Teacher";
+    window_option[2]="Delete Teacher";
+    window_option[3]="View Teacher's Info";
 
-    gotoxy(10,5);
-    cout<<"1.Add Teacher";
-    gotoxy(10,6);
-    cout<<"2.Modify Teacher";
-    gotoxy(10,7);
-    cout<<"3.Delete Teacher";
-    gotoxy(10,8);
-    cout<<"4.View Teacher's Info";
-
-    gotoxy(15,10);
-    cout<<"Enter your choice: ";
-    cin>>choice;
+    choice = option_input_on_window(window_option,4);
 
     if(choice==1)
     {
-        system("cls");
+        basic_window();
         gotoxy(10,5);
         cout<<"Enter New Teacher's ID: ";
         gotoxy(40,5);
@@ -37,7 +29,7 @@ void home_admin_control_teacher()
     }
     else if(choice==3)
     {
-        system("cls");
+        basic_window();
         gotoxy(10,5);
         cout<<"Enter New Teacher's ID: ";           //modification
         gotoxy(40,5);
@@ -49,9 +41,10 @@ void home_admin_control_teacher()
         //cout<<filename;
         if(remove(filename)==0)
         {
-            cout<<"The Teacher removed Succecfully";
+            confirmation_popup("The Teacher removed Succecfully");
         }
         else
-            cout<<"Couldn't remove the teacher";
+            confirmation_popup("Couldn't remove the teacher");
+
     }
 }
