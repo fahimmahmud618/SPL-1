74206a12df65dac4804f1cc6e29d2fdfca5e2370
fahimmahmud618 <75488772+fahimmahmud618@users.noreply.github.com>
2022-03-27 21:28:34 +0600

    Update checkallthing.cpp

--- checkallthing.cpp
+++ checkallthing.cpp
@@ -3,29 +3,105 @@
 #include<conio.h>
 #include<fstream>
 using namespace std;
-class student{
+
+class course
+{
 public:
-    char name[30];
-    char id[4];
-    char address[30];
+        float continious_evolution_total_mark;
+        float course_total_mark;
+        float class_test=0.0;
+        float midterm_mark;
+        float final_mark;
+        string course_name;
 
-    void display()
+    course()
     {
-        cout<<"Student Name: "<<name<<endl<<"ID: "<<id<<endl<<"Address: "<<address<<endl<<endl;
+        class_test=0;
+    }
+    //calculate total in continious evolution
+    float continious_evolution_total()
+    {
+        //return assignment+attendance+class_test;
     }
 
-    void read_data()
+    float course_total()
     {
-        cout<<"Enter name: ";
-        cin>>name;
+        return continious_evolution_total()+midterm_mark+final_mark;
+    }
+};
 
-        cout<<"Enter id: ";
+class semester
+{
+public:
+    course courses[6];
+};
+class academic
+{
+public:
+    semester semesters[8];
+};
+class person
+{
+public:
+    string name;
+    string id;
+    string address;
+    int age;
+
+    /*person(string Name, string Id, string Address, int Age)
+    {
+        name= Name;
+        Id= id;
+        address=Address;
+        age=Age;
+    }*/
+
+    void displayInformation()
+    {
+        //basic_window();
+        cout<<name;
+        cout<<id;
+        cout<<address;
+        cout<<age;
+    }
+    void SetInformation()
+    {
+        cout<<"Name : ";
+        getline(cin>>ws,name);
+        cout<<"ID : ";
         cin>>id;
-
-        cout<<"Enter address: ";
-        cin>>address;
+        cout<<"Address : ";
+        getline(cin>>ws,address);
+        cout<<"Age : ";
+        cin>>age;
 
     }
+
+
+
+};
+
+class student:public person, public academic
+{
+
+public:
+    academic a;
+    person p;
+    int current_semister;
+
+    //SetInformation();
+
+    void displayInformation()
+    {
+        //basic_window();
+        cout<<name;
+        cout<<id;
+        cout<<address;
+        cout<<age;
+        cout<<current_semister;
+        cout<<endl<<semesters[0].courses[2].class_test;
+    }
+
     void storedata()
     {
         ofstream out("sample.dat",ios::binary);
@@ -44,36 +120,71 @@ public:
         //in.seekg(sizeof(*this));
         //cout<<"___"<<in.tellg();
         in.read((char*)this,sizeof(*this));
-        display();
+        displayInformation();
 
 
         in.close();
     }
+    student()
+    {
 
+    }
+
+
+    student(string taken_id,int given_current_semister)
+    {
+        name="No name";
+        address="Unknown/no data";
+        age=0;
+        id=taken_id;
+        current_semister= given_current_semister;
+    }
+
+    void set_Student_info_basic(string taken_name, string taken_address, string taken_id, int taken_age, int taken_current_semister)
+    {
+        name=taken_name;
+        address = taken_address;
+        id=taken_address;
+        age = taken_age;
+        current_semister = taken_current_semister;
+    }
+
+    void promote_or_demote_student(string taken_student_filename,bool action)
+    {
+        ifstream in(taken_student_filename,ios::binary);
+        if(in)
+        {
+            in.read((char*)this,sizeof(*this));
+            if(action)
+                current_semister++;
+            else
+                current_semister--;
+        }
+        else
+            cout<<"Error happended";
+
+        ofstream out(taken_student_filename,ios::binary);
+        if(out)
+        {
+            out.write((char*)this,sizeof(*this));
+            out.close();
+        }
+        else
+            cout<<"Error happended";
+    }
 };
 
+class teacher:public person
+{
+public:
+    student s1;
+};
 
 int main()
 {
-    char word;
-    int t;
-    //ofstream f("sample.txt");
-    student s1,s2,s3;
-    //s1.read_data();
-    //s2.read_data();
-    //s3.read_data();
-    //s1.viewdata();
-
-    s2.viewdata();
-    s3.viewdata();
-    /*while(f.tellg()<7)
-    {
-        if(f.tellg()>3)
-        {f>>noskipws>>word;
-        cout<<word;}
-        //cout<<"__"<<f.tellg()<<"__";
-    }*/
-
-
-
+    student s1;
+    s1.current_semister=3;
+    s1.SetInformation();
+    s1.storedata();
+    s1.displayInformation();
 }
