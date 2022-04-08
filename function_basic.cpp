8b5d5b5bd80aa43299d76d367c919b924c83047f
fahimmahmud618 <75488772+fahimmahmud618@users.noreply.github.com>
2022-04-08 19:18:36 +0600

    Update function_basic.cpp

--- function_basic.cpp
+++ function_basic.cpp
@@ -26,6 +26,55 @@ bool check_number_or_not(string input)
     return true;
 }
 
+int convert_stringToNumber(string s)
+{
+    int num = 0;
+    int n = calc_string_len(s);
+
+    for (int i = 0; i < n; i++)
+        num = num * 10 + (int(s[i]) - 48);
+
+    return num;
+}
+
+string convert_numberToString(int number)
+{
+    int n = INT_MIN;
+    char buffer[50];
+    int i = 0;
+
+    bool isNeg = n<0;
+
+    unsigned int n1 = isNeg ? -n : n;
+
+    while(n1!=0)
+    {
+        buffer[i++] = n1%10+'0';
+        n1=n1/10;
+    }
+
+    if(isNeg)
+        buffer[i++] = '-';
+
+    buffer[i] = '\0';
+
+    for(int t = 0; t < i/2; t++)
+    {
+        buffer[t] ^= buffer[i-t-1];
+        buffer[i-t-1] ^= buffer[t];
+        buffer[t] ^= buffer[i-t-1];
+    }
+
+    if(n == 0)
+    {
+        buffer[0] = '0';
+        buffer[1] = '\0';
+    }
+
+    printf(buffer);
+
+}
+
 double string_to_number(string taken_string)
 {
     double num;
