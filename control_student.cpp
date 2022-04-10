fce3d3ed764b6be36625fa344a93fb4f0f5999bb
fahimmahmud618 <75488772+fahimmahmud618@users.noreply.github.com>
2022-04-10 20:15:13 +0600

    Update control_student.cpp

--- control_student.cpp
+++ control_student.cpp
@@ -183,15 +183,16 @@ void update_continious_evolution_mark(string person_id)
 {
     string window_option[100];
     int i=0;
+    basic_window();
+    vector<int> choice=ret_selected_course_num(ret_selected_course_of_teacher(person_id));
+    vector<string> student_files;
+    student s;
+
+    string batch_serial = ret_batchSerial(to_string(choice[0]));
+    string filename = ".//studentData//"+batch_serial, temp_string;      //modification needed
+
     if(two_confirmation_popup("How do you want to update the mark?","Batchwise","Personally"))
     {
-        basic_window();
-        vector<int> choice=ret_selected_course_num(ret_selected_course_of_teacher(person_id));
-        vector<string> student_files;
-        student s;
-
-        string batch_serial = ret_batchSerial(to_string(choice[0]));
-        string filename = ".//studentData//"+batch_serial;      //modification needed
         cout<<filename;
         student_files  = list_of_files(filename);
         for(auto student_file : student_files)
@@ -202,7 +203,8 @@ void update_continious_evolution_mark(string person_id)
         vector<string> taken_marks = taking_list_input_on_window(window_option,i,1);
         for(i=0;i<student_files.size();i++)
         {
-            s.upadate_continious_evolution_mark(student_files[i],stof(taken_marks[i]),choice[0],choice[1]);
+            temp_string = filename+student_files[i];
+            s.upadate_continious_evolution_mark(temp_string,stof(taken_marks[i]),choice[0],choice[1]);
 
         }
         /*for((auto student_file1 : student_files)&&( auto mark : taken_marks))
@@ -210,6 +212,16 @@ void update_continious_evolution_mark(string person_id)
             s.upadate_continious_evolution_mark(student_file1,stof(mark),choice[0],choice[1]);
         }*/
         //s.upadate_continious_evolution_mark(func_makeFilename("12","1201"),2.3,choice[0],choice[1]);
+    }
+    else
+    {
+        basic_window();
+        color(7);
+        window_option[0]="Enter roll no: ";
+        window_option[1]="Enter mark: ";
+        vector<string> inputs = taking_list_input_on_window(window_option,2,2);
+        filename = filename+"//"+inputs[0]+".dat";
+        s.upadate_continious_evolution_mark(filename,stof(inputs[1]),choice[0],choice[1]);
     }
 
 }
