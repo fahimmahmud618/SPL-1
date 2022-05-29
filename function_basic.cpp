b4368fcc92c810218283a1ea27f2b2558e19bfc1
fahimmahmud618 <75488772+fahimmahmud618@users.noreply.github.com>
2022-05-29 23:06:33 +0600

    Update function_basic.cpp

--- function_basic.cpp
+++ function_basic.cpp
@@ -1,126 +1,159 @@
 #include "header.h"
-
-void gotoxy(int x, int y)
-{
-    COORD c;
-    c.X = x;
-    c.Y = y;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),c);
-}
-
-int calc_string_len(string input)
-{
-    int i;
-    for(i=0;input[i]!='\0';i++);
-    return i;
-}
-
-bool check_number_or_not(string input)
-{
-    int i;
-    for(i=0;i<calc_string_len(input);i++)
+//#include "b_plus_tree.h"
+//using namespace std;
+/*struct Hash {
+    const int p = 31, m = 1e9 + 7;
+    int hash_value;
+    Hash(const string& s)
     {
-        if(isdigit(input[i])==false)
-            return false;
-    }
-    return true;
-}
-
-int convert_stringToNumber(string s)
-{
-    int num = 0;
-    int n = calc_string_len(s);
-
-    for (int i = 0; i < n; i++)
-        num = num * 10 + (int(s[i]) - 48);
-
-    return num;
-}
-
-int string_to_number(string taken_string)
-{
-    double num;
-    num = stoi(taken_string);
-    return num;
-}
-
-bool encrypt(string taken_file)
-{
-    char ch,oldname[]="temp" ;
-    char* newname=&taken_file[0];
-    int t;
-    ifstream input;
-    ofstream output;
-
-    input.open(taken_file, ios:: binary);
-    output.open("temp",ios::binary);
-
-    if((input)&&(output))
-    {
-        while(!input.eof())
+        int previous_hash_value = 0;
+        long p_pow = 1;
+        const int n = calc_string_len(s);
+        for (int i = 0; i < n; ++i)
         {
-            input>>noskipws>>ch;
-            t=ch+100;
-            output<<(char)t;
+                previous_hash_value = (previous_hash_value + (s[i] - 'a' + 1) * p_pow) % m;
+                p_pow = (p_pow * p) % m;
         }
-        input.close();
-        output.close();
-        remove(&taken_file[0]);
-        rename(oldname,newname);
-
-        return true;
+        hash_value = previous_hash_value;
     }
+
+    bool operator==(const Hash& other) {
+        return (hash_value == other.hash_value);
+    }
+};*/
+
+vector<string> list_of_files(string folder)
+{
+    vector<string> names;
+    string search_path = folder + "/*.*";
+    WIN32_FIND_DATA fd;
+    HANDLE hFind = ::FindFirstFile(search_path.c_str(), &fd);
+    if(hFind != INVALID_HANDLE_VALUE)
+        {
+        do
+        {
+
+            if(! (fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) )
+            {
+                names.push_back(fd.cFileName);
+            }
+        }while(::FindNextFile(hFind, &fd));
+        ::FindClose(hFind);
+    }
+    return names;
+}
+
+/*bool check_file_if_exits(string filename, string folderName)
+{
+    int temp_hash_value;
+    vector<string> filenames = list_of_files(folderName);
+    BPlusTree bPlusTree;
+
+    for(auto E : filenames)
+    {
+        Hash h(E);
+        temp_hash_value = h.hash_value;
+        bPlusTree.insertNode(temp_hash_value);
+    }
+
+    Hash h1(folderName);
+    temp_hash_value = h1.hash_value;
+    if(bPlusTree.search(temp_hash_value))
+        return true;
     else
         return false;
 }
-
-bool decrypt(string taken_file)
+*/
+bool check_file_if_exits(string filename, string folderName)
 {
+    vector<string> filenames = list_of_files(folderName);
 
-
-    char ch,oldname[]="temp" ;
-    char* newname=&taken_file[0];
-    int t;
-    ifstream input;
-    ofstream output;
-
-    input.open(taken_file, ios:: binary);
-    output.open("temp",ios::binary);
-
-    if((input)&&(output))
+    for(auto E : filenames)
     {
-        while(!input.eof())
-        {
-            input>>noskipws>>ch;
-            t=ch-100;
-            output<<(char)t;
-        }
-        input.close();
-        output.close();
-        remove(&taken_file[0]);
-        rename(oldname,newname);
-
-        return true;
+        if(E==filename)
+            return true;
     }
-    else
-        return false;
+
+    return false;
+}
+vector<string> ret_selected_course_of_teacher(string teacher_id)
+{
+    string taken_teacher_id,temp_string;
+    fstream coursefile;
+    vector<string> coursesFromFile;
+
+    coursefile.open(".//academic//teacher_assign_to_course.txt");
+
+    while(coursefile.eof()==0)
+    {
+        getline(coursefile,taken_teacher_id);
+        if(taken_teacher_id==teacher_id)
+        {
+            while((coursefile.peek()!='\n')&&(coursefile>>temp_string))
+            coursesFromFile.push_back(temp_string);
+        }
+    }
+    return coursesFromFile;
 }
 
-bool if_contains_other_char(string given_string)
+vector<int> ret_selected_course_num(vector<string> courses)
 {
-    int i,length = calc_string_len(given_string);
-
-    for(i=0;i<length;i++)
+    string temp, option[10],t1,t2;
+    vector<int> outputs;
+    int i=0,size = courses.size();
+    for(auto t:courses)
     {
-        if(((given_string[i]>='a')&&(given_string[i]<='z'))||((given_string[i]>='A')&&(given_string[i]<='Z'))||((given_string[i]>='0')&&(given_string[i]<='9')))
-            continue;
-        else
+        t1.clear();
+        t2.clear();
+        t1.push_back(t[0]);
+        t1.push_back(t[2]);
+        option[i]="Semister: "+t1+" Course: "+t2;
+        i++;
+    }
+    int choice = option_input_on_window(option,i,2);
+    temp = courses[choice-1];
+    outputs.push_back((int)temp[0]);
+    outputs.push_back((int)temp[2]);
+
+    return outputs;
+}
+
+string func_makeFilename(string batch_serial, string student_id)
+{
+    string new_student_id,makefilename;
+
+    if(convert_stringToNumber(student_id)<10)
+        new_student_id=batch_serial+"//"+batch_serial+"0"+student_id+".dat";
+    else
+        new_student_id=batch_serial+"//"+batch_serial+student_id+".dat";
+
+    makefilename=".//studentData//batch"+new_student_id;
+
+    return makefilename;
+}
+
+string ret_batchSerial(string semister_num)
+{
+    fstream file;
+    string temp_string;
+    file.open(".//academic//batchwise_semister.txt");
+    while(file.eof()==0)
+    {
+        getline(file,temp_string);
+        if(temp_string==semister_num)
+        {
+            getline(file,temp_string);
             break;
+        }
     }
-    if(i==length)
-        return true;
-    else
-        return false;
+    return temp_string;
 }
 
+string retfileNameFromID(string id)
+{
+    string a;
+    a.push_back(id[0]);
+    a.push_back(id[1]);
+    return ".//studentData//batch"+a+"//"+id+".dat";
+}
 
